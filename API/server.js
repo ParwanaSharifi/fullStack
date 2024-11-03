94d8fb0e37d8ce787529ec32ae5ae4c3f2e41392
ParwanaSharifi <116257887+ParwanaSharifi@users.noreply.github.com>
2024-11-03 08:49:40 -0800

    Update server.js

--- API/server.js
+++ API/server.js
@@ -35,5 +35,8 @@ mongoose
 const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Food Recipe API!");
+});
 
 
